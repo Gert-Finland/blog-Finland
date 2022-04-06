7ae47d77e08aef64cf50f89e49b384a96a66963e
Gert-Finlan <mute.wake_0w@icloud.com>
2022-04-06 19:18:06 +0300

    blog week7, improved mobile design

--- scripts/showMoreOption.js
+++ scripts/showMoreOption.js
@@ -13,3 +13,19 @@ function ShowMoreImages() {
         moreImg.style.display = "inline";
     }
 }
+
+function ShowMoreImages2() {
+    var divider = document.getElementById("divider2");
+    var moreImg = document.getElementById("moreImg2");
+    var btnText = document.getElementById("imgBtn2");
+
+    if (divider.style.display === "none") {
+        divider.style.display = "inline";
+        btnText.innerHTML = "Toon meer";
+        moreImg.style.display = "none";
+    } else {
+        divider.style.display = "none";
+        btnText.innerHTML = "Toon minder";
+        moreImg.style.display = "inline";
+    }
+}
