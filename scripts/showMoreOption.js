43d160a4a42de0603159114724a566d9176e6b3f
Gert-Finlan <mute.wake_0w@icloud.com>
2022-04-07 18:37:54 +0300

    Language mistakes blog 7 corrected, fixed show more btn blog 7, fixed video and header img not showing

--- scripts/showMoreOption.js
+++ scripts/showMoreOption.js
@@ -15,17 +15,17 @@ function ShowMoreImages() {
 }
 
 function ShowMoreImages2() {
-    var divider = document.getElementById("divider2");
-    var moreImg = document.getElementById("moreImg2");
-    var btnText = document.getElementById("imgBtn2");
+    var divider2 = document.getElementById("divider2");
+    var moreImg2 = document.getElementById("moreImg2");
+    var btnText2 = document.getElementById("imgBtn2");
 
-    if (divider.style.display === "none") {
-        divider.style.display = "inline";
-        btnText.innerHTML = "Toon meer";
-        moreImg.style.display = "none";
+    if (divider2.style.display === "none") {
+        divider2.style.display = "inline";
+        btnText2.innerHTML = "Toon meer";
+        moreImg2.style.display = "none";
     } else {
-        divider.style.display = "none";
-        btnText.innerHTML = "Toon minder";
-        moreImg.style.display = "inline";
+        divider2.style.display = "none";
+        btnText2.innerHTML = "Toon minder";
+        moreImg2.style.display = "inline";
     }
 }
